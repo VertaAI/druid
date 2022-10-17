a64eff0e1bc898d3da9322d0d6811040b8c7a485
Liam Newman <liam@verta.ai>
2022-10-17 13:21:00 -0700

    Test simple snapshot build

--- verta_build_and_publish.sh
+++ verta_build_and_publish.sh
@@ -8,13 +8,13 @@ export VERSION_SUFFIX=$(echo $VERSION_SUFFIX | sed 's,/,-,g' | tr '[:upper:]' '[
 export PROJECT_VERSION=$(mvn help:evaluate -Dexpression=revision -q -DforceStdout)
 export PROJECT_VERSION=$(echo $PROJECT_VERSION | sed "s,-SNAPSHOT,-${VERSION_SUFFIX}-SNAPSHOT,g")
 export MAVEN_PARAMS='-Pdist-hadoop3,hadoop3,bundle-contrib-exts -Dpmd.skip=true -Denforcer.skip -Dforbiddenapis.skip=true -Dcheckstyle.skip=true -Danimal.sniffer.skip=true -Djacoco.skip=true -DskipTests'
-mvn -B versions:set -DnewVersion=$PROJECT_VERSION > /dev/null
+#mvn -B versions:set -DnewVersion=$PROJECT_VERSION > /dev/null
 mvn -B deploy $MAVEN_PARAMS || {
-    mvn -B versions:revert > /dev/null
+#    mvn -B versions:revert > /dev/null
     # strange behavior where website needs separate call to revert
-    `cd website && mvn -B versions:revert > /dev/null`
+#    `cd website && mvn -B versions:revert > /dev/null`
     exit 1
 }
-mvn -B versions:revert > /dev/null
+#mvn -B versions:revert > /dev/null
 # strange behavior where website needs separate call to revert
-`cd website && mvn -B versions:revert > /dev/null`
+#`cd website && mvn -B versions:revert > /dev/null`
