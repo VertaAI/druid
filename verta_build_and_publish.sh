dcfb5f1d244b2679e77e7c08d5ff203eaaaf867a
Liam Newman <liam@verta.ai>
2022-10-17 12:42:39 -0700

    lowercase branch name

--- verta_build_and_publish.sh
+++ verta_build_and_publish.sh
@@ -4,7 +4,7 @@ set -eo pipefail
 
 export JAVA_TOOL_OPTIONS=-Dfile.encoding=UTF8
 export VERSION_SUFFIX="$(git rev-parse --abbrev-ref HEAD)"
-export VERSION_SUFFIX=$(echo $VERSION_SUFFIX | sed 's,/,-,g')
+export VERSION_SUFFIX=$(echo $VERSION_SUFFIX | sed 's,/,-,g' | tr '[:upper:]' '[:lower:]')
 export PROJECT_VERSION=$(mvn help:evaluate -Dexpression=revision -q -DforceStdout)
 export PROJECT_VERSION=$(echo $PROJECT_VERSION | sed "s,-SNAPSHOT,-${VERSION_SUFFIX}-SNAPSHOT,g")
 export MAVEN_PARAMS='-Pdist-hadoop3,hadoop3,bundle-contrib-exts -Dpmd.skip=true -Denforcer.skip -Dforbiddenapis.skip=true -Dcheckstyle.skip=true -Danimal.sniffer.skip=true -Djacoco.skip=true -DskipTests'
