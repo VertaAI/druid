0d7759b3b9b1af443ea1b1a513440b6a8fcd0a98
John K Watson <104948055+jkwatson-verta@users.noreply.github.com>
2023-01-24 12:51:53 -0800

    Try adding some additional sql server wrapping

--- extensions-contrib/sqlserver-metadata-storage/src/main/java/org/apache/druid/metadata/storage/sqlserver/SQLServerConnector.java
+++ extensions-contrib/sqlserver-metadata-storage/src/main/java/org/apache/druid/metadata/storage/sqlserver/SQLServerConnector.java
@@ -236,7 +236,7 @@ public class SQLServerConnector extends SQLMetadataConnector
             handle.createStatement(StringUtils.format(
                 "MERGE INTO %1$s WITH (UPDLOCK, HOLDLOCK) as target"
                     + " USING "
-                    + " (:key, :value) as source (%2$s, %3$s)"
+                    + " values((:key, :value)) as source (%2$s, %3$s)"
                     + " ON"
                     + " (target.%2$s = source.%2$s)"
                     + " WHEN MATCHED THEN UPDATE SET %3$s = :value"
